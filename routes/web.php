6296c7e90f05b679ce9eab15ba00f5949dfd4397
Aliya <aliya.genius@gmail.com>
2022-11-01 11:17:41 +0800

    Backend homepage slider option 3

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,7 @@ Route::controller(AdminController::class)->group(function () {
 // HomeSlide routes
 Route::controller(HomeSliderController::class)->group(function () {
     Route::get('/home/slide', 'HomeSlider')->name('home.slide');
+    Route::post('/update/slider', 'UpdateSlider')->name('update.slider');
 });
 
 
