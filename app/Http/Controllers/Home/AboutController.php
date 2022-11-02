2577a41b6fdcb811721647f85c09cda7b8c10cd0
Aliya <aliya.genius@gmail.com>
2022-11-02 09:04:29 +0800

    Frontend About page 2

--- app/Http/Controllers/Home/AboutController.php
+++ app/Http/Controllers/Home/AboutController.php
@@ -57,4 +57,10 @@ class AboutController extends Controller
             return redirect()->back()->with($notification);
         }
     }
+
+    public function HomeAbout()
+    {
+        $aboutpage = About::find(1);
+        return view('frontend.about_page', compact('aboutpage'));
+    }
 }
